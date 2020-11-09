067a186961b4d7a49d8200c1c256a50c38deb342
lazarusWist <lawrence.witt@hotmail.co.uk>
2020-11-09 16:39:21 +0000

    0.0.2 - add commit-version body support

--- build_scripts/commit-version.js
+++ build_scripts/commit-version.js
@@ -18,9 +18,10 @@ const appVersion = packageJson.version;
 
 const TYPE = process.env.TYPE;
 const SUM = process.env.SUM;
-let BODY = process.env.BODY;
+const BODY = process.env.BODY;
 
 const typeSchema = { major: 0, minor: 1, patch: 2 };
+let commitBody;
 
 if (!TYPE || !Object.keys(typeSchema).includes(TYPE)) {
     throw new Error("Invalid or missing version type.");
@@ -32,12 +33,15 @@ if (!SUM) {
 
 if (BODY) {
     try {
-        BODY = fs.readFileSync(path.join(__dirname, BODY), 'utf-8');
-    } catch (e) {
-        throw new Error("A body file was specified but could not be read. Check it exists in the same directory and includes a file extension.");
+        commitBody = fs.readFileSync(path.join(__dirname, BODY), 'utf-8');
+        if (commitBody.includes('#!#')) {
+            throw new Error("Delete template comment and ruler before use");
+        };
+        fs.unlinkSync(path.join(__dirname, BODY));
+    } catch (error) {
+        throw new Error(`A body file was specified but could not be parsed: ${error.message}.`);
     };
 };
-
 
 /* Generate New Version */
 
@@ -62,6 +66,7 @@ if (commitSummary.length > 72) {
     throw new Error(`Commit summaries should be <= 72 characters. Current length including version number: ${commitSummary.length}.`);
 };
 
+const commitMessage = `${commitSummary}${commitBody ? `\n${commitBody}` : ""}`;
 
 /* Update package.json, package-lock.json, meta.json */
 
@@ -85,7 +90,9 @@ Object.keys(updateSchema).forEach(path => {
     };
 });
 
+
 /* Stage and commit all changes with supplied git summary and body */
+
 exec("git add -A", error => {
     if (error) {
         console.log(`Error staging version changes: ${error.message}.`);
@@ -93,9 +100,9 @@ exec("git add -A", error => {
     };
     console.log(`Version ${newVersion} changes staged.`);
 
-    exec(`git commit -m "${commitSummary}" ${BODY ? `-m "${BODY}"` : ""}`, error => {
+    exec(`git commit -m $'${commitMessage}'`, error => {
         if (error) {
-            console.log(`Error committing version changes: ${error.message}`);
+            console.log(`Error committing version changes: ${error.message}.`);
             return;
         };
         console.log(`Version ${newVersion} changes committed.`);
